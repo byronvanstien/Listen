d5ab3a9f460464ae754b10f033f3d20fc51a2660
Byron Vanstien <recchan@protonmail.com>
2017-07-25 19:37:31 +0000

    Changed setup.py to be consistent with use of ""

--- setup.py
+++ setup.py
@@ -11,21 +11,21 @@ from pyanimelist import (
 setup(
     name=title,
     version=version,
-    packages=find_packages(),
+    packages=find_packages("listen"),
     url=LIBRARY_URL,
     license=license,
     author=author,
-    author_email='',
-    description='Python 3 bindings for the MyAnimeList API.',
-    long_description='An asynchronous wrapper for the MyAnimeList api.',
+    author_email="",
+    description="Python 3 bindings for the Listen.moe API.",
+    long_description="An asynchronous wrapper for the Listen.moe api.",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Software Development :: Libraries'
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries"
     ],
-    keywords="aiohttp asyncio myanimelist xml parsing",
-    install_requires=['aiohttp', 'bs4', 'lxml', 'dicttoxml'],
+    keywords="aiohttp asyncio listen.moe",
+    install_requires=["aiohttp"],
 )
